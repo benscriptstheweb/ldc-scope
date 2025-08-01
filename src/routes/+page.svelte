d568a012e7278133360e06ed6c80eaa53c632873
Robin Jalandoni <robinjalandoni@gmail.com>
2025-08-01 08:57:27 -0400

    convert to server side firebase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,11 @@
 <script lang="ts">
-	import { getHomes } from '$lib/controllers/homes';
-	import Loading from '../components/Loading.svelte';
 	import HomeCard from '../components/HomeCard.svelte';
+
+	export let data;
 </script>
 
-<div>
-	{#await getHomes()}
-		<Loading />
-	{:then homes}
-		{#each homes as home}
-			<HomeCard {home} />
-		{/each}
-	{/await}
-</div>
+<p class="heading">Inventory</p>
+
+{#each data.homes as home}
+	<HomeCard {home} />
+{/each}
