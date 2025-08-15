09d5425bba9a8a9e54f76da6267adb712e65477f
Robin Jalandoni <robinjalandoni@gmail.com>
2025-08-15 14:32:40 -0400

    remove unused api call

--- src/routes/homes/[id]/+page.server.ts
+++ src/routes/homes/[id]/+page.server.ts
@@ -4,9 +4,6 @@ export const load: PageServerLoad = async ({ params, fetch }) => {
     const homeResponse = await fetch(`/api/homes/${params.id}`);
     const homeById = await homeResponse.json();
 
-    const assignmentsRes = await fetch(`/api/homes/${params.id}/assignedVolunteers`);
-    const assignments = await assignmentsRes.json();
-
     let home = {
         id: homeById.id,
         ...homeById,
