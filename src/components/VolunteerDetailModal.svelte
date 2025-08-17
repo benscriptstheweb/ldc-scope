ef2a62a5d57d9ce39a619d8352eb179c46447597
Robin Jalandoni <robinjalandoni@gmail.com>
2025-08-17 11:16:38 -0400

    create volunteer editor

---
+++ src/components/VolunteerDetailModal.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let { volunteerDetail, id } = $props();
+</script>
+
+<dialog {id} class="modal">
+	<div class="modal-box">
+		<p class="heading">Volunteer</p>
+
+		<h2>{volunteerDetail.name}</h2>
+
+		<div class="modal-action">
+			<form method="dialog">
+				<button class="btn">Close</button>
+			</form>
+		</div>
+	</div>
+</dialog>
