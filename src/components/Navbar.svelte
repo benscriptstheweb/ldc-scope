f8f70d34d05a8658b92ce736ec5d974106b2f17f
Robin Jalandoni <robinjalandoni@gmail.com>
2025-08-26 20:07:30 -0400

    add indicator for unassigned vols

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -2,11 +2,17 @@
 	import { auth } from '$lib/firebase/client';
 	import { signOut } from 'firebase/auth';
 
-	export let userData;
+	let { userData } = $props();
 
 	async function logout() {
 		await fetch('/api/sessionLogout', { method: 'POST' });
 		await signOut(auth);
+	}
+
+	async function getUnassignedCount() {
+		const res = await fetch('/api/volunteers');
+		const volunteers = await res.json();
+		return volunteers.filter((v) => v.isAssigned === 0).length;
 	}
 </script>
 
@@ -42,25 +48,30 @@
 			<!-- link to volunteers page -->
 			<li>
 				<a href="/volunteers">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="size-6"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
-						/>
-					</svg>
-					<span class="menu-text">Volunteers</span>
+					<div class="indicator">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="size-6"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+							/>
+						</svg>
+						<p class="menu-text">Volunteers</p>
+						{#await getUnassignedCount() then count}
+							<span class="badge badge-xs badge-secondary indicator-item">{count}</span>
+						{/await}
+					</div>
 				</a>
 			</li>
 			<li>
-				<details>
+				<details class="dropdown">
 					<summary>
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
@@ -79,7 +90,7 @@
 						<span class="menu-text">Account</span>
 					</summary>
 
-					<ul class="bg-base-100 rounded-t-none p-2">
+					<ul class="bg-base-100 rounded-t-none p-2 dropdown-content menu">
 						<li><button on:click={logout}>Logout</button></li>
 						<li class="menu-disabled"><button>Settings</button></li>
 						<div class="divider"></div>
