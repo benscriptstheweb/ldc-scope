2ff7c28092b111c834f40b466cf60e644d062d23
Robin Jalandoni <robinjalandoni@gmail.com>
2025-08-29 20:29:13 -0400

    prevent double booking home

--- src/routes/api/assignments/+server.ts
+++ src/routes/api/assignments/+server.ts
@@ -1,6 +1,13 @@
 import { json } from '@sveltejs/kit';
 import { supabase } from '$lib/supabase/supabaseClient';
 
+type Volunteer = {
+    volunteer_id: {
+        date_start: string;
+        date_end: string;
+    }
+}
+
 export async function GET() {
     const { data: assignments, error } = await supabase
         .from('assignments')
@@ -20,6 +27,33 @@ export async function POST({ locals, request }) {
     }
 
     const body = await request.json();
+
+    // STEP 1: get the dates of the volunteer
+    const { data, error: noVolunteer } = await supabase
+        .from('volunteers')
+        .select('date_start, date_end')
+        .eq('id', body.volunteerId)
+        .single();
+
+    if (noVolunteer) {
+        return json({ error: noVolunteer.message }, { status: 500 });
+    };
+
+    const { date_start, date_end } = data;
+
+    const { data: preAssignedVolunteers } = await supabase
+        .from('assignments')
+        .select(`volunteer_id ( date_start, date_end )`)
+        .eq('home_id', body.homeId)
+        .overrideTypes<Volunteer[]>();
+
+    const arrStartDates = preAssignedVolunteers?.map(e => new Date(e.volunteer_id.date_start).getTime())
+    const arrEndDates = preAssignedVolunteers?.map(e => new Date(e.volunteer_id.date_end).getTime())
+
+    if (arrStartDates?.some(s => s <= new Date(date_end).getTime())
+        && arrEndDates?.some(e => e >= new Date(date_start).getTime())) {
+        return json({ error: 'Overlapping dates' }, { status: 500 });
+    }
 
     const { error } = await supabase
         .from('assignments')
