6f7e2974043fdffc35079c269da19523d54f6494
Robin Jalandoni <robinjalandoni@gmail.com>
2025-10-06 20:03:48 -0400

    fix id from delete confirmation

--- src/components/DeleteConfirm.svelte
+++ src/components/DeleteConfirm.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
-	let { deleteFunction, id } = $props();
+	let {
+		deleteFunction,
+		id,
+		message = 'Are you sure you want to delete this item? This action cannot be undone.'
+	} = $props();
 </script>
 
 <dialog {id} class="modal">
 	<div class="modal-box">
 		<h3 class="text-lg font-bold">Confirm Delete</h3>
-		<p>Are you sure you want to delete this item? This action cannot be undone.</p>
+		<p>{message}</p>
 
 		<div class="modal-action">
 			<button
-				onclick={() => (document.getElementById('delete-confirm') as HTMLDialogElement)?.close()}
+				onclick={() => (document.getElementById(id) as HTMLDialogElement)?.close()}
 				class="btn">Cancel</button
 			>
 			<button onclick={deleteFunction} class="btn btn-soft btn-error">Delete</button>
