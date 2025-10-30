baf0ec5127e6b1fd989b88a28e1841052a4b9329
Robin Jalandoni <robinjalandoni@gmail.com>
2025-10-30 19:13:24 -0400

    new icons

---
+++ src/icons/Logout.svelte
@@ -0,0 +1,5 @@
+<svg width="23" height="23" viewBox="0 0 300 309" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<rect width="199.568" height="308.456" fill="#B4B4B4" />
+	<rect x="99.7838" width="199.568" height="308.456" fill="#D9D9D9" />
+	<ellipse cx="271.13" cy="154.741" rx="18.1425" ry="18.4459" fill="#B4B4B4" />
+</svg>
