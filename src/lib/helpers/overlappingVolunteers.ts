001f599be71776831a7bfeb2c57cb5de13b8d183
Robin Jalandoni <robinjalandoni@gmail.com>
2026-01-18 09:21:32 -0500

    better type and comments for overlapping volunteer helper

--- src/lib/helpers/overlappingVolunteers.ts
+++ src/lib/helpers/overlappingVolunteers.ts
@@ -1,28 +1,30 @@
 import { supabase } from "$lib/supabase/supabaseClient";
-
-type Volunteer = {
-    volunteer_id: {
-        date_start: string;
-        date_end: string;
-    }
-}
+import { type Assignment } from "$lib/supabase/types/assignment";
 
 export async function isOverlapping(volunteerId: string, homeId: string) {
+    // 1. get volunteers
     const { data } = await supabase
         .from('volunteers')
         .select('date_start, date_end')
         .eq('id', volunteerId)
         .single();
 
+    // 2. get assignments
     const { data: preAssignedVolunteers } = await supabase
         .from('assignments')
         .select(`volunteer_id ( date_start, date_end )`)
         .eq('home_id', homeId)
-        .overrideTypes<Volunteer[]>();
+        .overrideTypes<Assignment[]>();
 
-    const arrStartDates = preAssignedVolunteers?.map(e => new Date(e.volunteer_id.date_start).getTime())
-    const arrEndDates = preAssignedVolunteers?.map(e => new Date(e.volunteer_id.date_end).getTime())
+    // 3. get all start and end dates of the volunteers
+    const arrStartDates = preAssignedVolunteers?.map(assignment => new Date(assignment.volunteer_id.date_start).getTime());
+    const arrEndDates = preAssignedVolunteers?.map(assignment => new Date(assignment.volunteer_id.date_end).getTime());
 
+    console.log(arrStartDates)
+
+    // 4. if any start date before another volunteer's end date and...
+    // 5. ...if any end date is after another volunteer's start date...
+    // ...then it is overlapping.
     if (data && arrStartDates?.some(s => s <= new Date(data.date_end).getTime())
         && arrEndDates?.some(e => e >= new Date(data.date_start).getTime())) {
         return true;
