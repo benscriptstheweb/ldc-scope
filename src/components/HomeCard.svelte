7c163ff9cfcf36abdf5acb24cbae50978dc5502c
Robin Jalandoni <robinjalandoni@gmail.com>
2026-02-09 18:30:57 -0500

    fix homecard spacing

--- src/components/HomeCard.svelte
+++ src/components/HomeCard.svelte
@@ -12,12 +12,8 @@
 </script>
 
 <div class="cards-container">
-	<button
-		type="button"
-		class="card shadow-md cursor-pointer w-85 text-left m-3"
-		onclick={visitHome}
-	>
-		<div class="card-body bg-base-200">
+	<button class="card shadow-md cursor-pointer w-81 mt-1 text-left" onclick={visitHome}>
+		<div class="p-3 bg-base-200">
 			<div class="flex justify-between">
 				<div>
 					<div class="indicator card-title">
@@ -27,17 +23,20 @@
 					{#if home.isHosting}
 						<div class="badge badge-sm badge-error badge-soft mt-1">Hosting</div>
 					{:else}
-						Available: {getParsedDate(home.date_available)}
+						<p class="half-address">Available: {getParsedDate(home.date_available)}</p>
 					{/if}
 				</div>
 
 				<!-- INDICATORS -->
-				<div class="flex items-center">
-					{#if home.hasPets}
-						<CustomBadge type="pet" />
-					{/if}
-					<CustomBadge type="days" days={home.max_days_stay} />
-					<RecommendedOccupantBadge occupantType={home.occupant_type} />
+				<div class="flex flex-col items-end justify-center">
+					<div class="flex items-center ml-3 mb-3">
+						{#if home.hasPets}
+							<CustomBadge type="pet" />
+						{/if}
+						<CustomBadge type="days" days={home.max_days_stay} />
+						<RecommendedOccupantBadge occupantType={home.occupant_type} />
+					</div>
+					<div class="badge badge-xs">{home.distance_to_project} mi</div>
 				</div>
 			</div>
 		</div>
@@ -47,6 +46,7 @@
 <style>
 	.half-address {
 		font-style: italic;
+		font-size: 0.9em;
 	}
 	.card-title {
 		font-weight: 800;
