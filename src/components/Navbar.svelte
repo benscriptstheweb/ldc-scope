4f63f19a4372f946484289573a3bcbbe5ce60a8c
Robin Jalandoni <robinjalandoni@gmail.com>
2026-02-27 15:40:35 -0500

    fix icon fill

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -28,6 +28,7 @@
 						width="22"
 						viewBox="0 0 24 24"
 						stroke="currentColor"
+						fill="none"
 						stroke-width="2"
 						stroke-linecap="round"
 						stroke-linejoin="round"
@@ -47,6 +48,7 @@
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							width="22"
+							fill="none"
 							viewBox="0 0 24 24"
 							stroke="currentColor"
 							stroke-width="2"
@@ -71,6 +73,7 @@
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						width="22"
+						fill="none"
 						viewBox="0 0 24 24"
 						stroke="currentColor"
 						stroke-width="2"
