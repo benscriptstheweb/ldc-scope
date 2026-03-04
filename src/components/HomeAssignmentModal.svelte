bf7a5a54b06fd99af647a5c13b5c0b606d67ef96
Robin Jalandoni <robinjalandoni@gmail.com>
2026-03-04 17:35:09 -0500

    add skeleton

--- src/components/HomeAssignmentModal.svelte
+++ src/components/HomeAssignmentModal.svelte
@@ -40,12 +40,12 @@
 
 	let startDate = $state(volunteerToAssign.date_start);
 	let endDate = $state(volunteerToAssign.date_end);
-	let assignableHomes: any[] = $state([]);
-	let unAssignableHomes: any[] = $state([]);
+	// let assignableHomes: any[] = $state([]);
+	// let unAssignableHomes: any[] = $state([]);
 
 	async function getUpdatedHomes(start: string, end: string) {
 		if (!start || !end) {
-			return;
+			return { assignableHomes: [], unAssignableHomes: [] };
 		}
 
 		const homes = await getHomesByVolunteerProject();
@@ -70,8 +70,7 @@
 			}
 		}
 
-		assignableHomes = newAssignable;
-		unAssignableHomes = newUnAssignable;
+		return { assignableHomes: newAssignable, unAssignableHomes: newUnAssignable };
 	}
 
 	$effect(() => {
@@ -94,78 +93,87 @@
 		</div>
 
 		<div class="divider"></div>
+		{#await getUpdatedHomes(startDate, endDate)}
+			<div class="skeleton h-4 w-60 mb-3"></div>
+			<div class="skeleton h-4 w-30 mb-3"></div>
+			<div class="skeleton h-4 w-40"></div>
+		{:then { assignableHomes, unAssignableHomes }}
+			{#if !assignableHomes.length && !unAssignableHomes.length}
+				There are no homes for this project 🏡
+			{/if}
 
-		{#if assignableHomes.length > 0}
-			<ul class="list mb-8">
-				<h2 class="subheading">Assignable homes</h2>
-				{#each assignableHomes as home}
-					<li class="list-row">
-						{home.address1}
-						<div>
-							<div class="badge badge-xs">{home.distance_to_project} mi</div>
-							<button
-								onclick={() => createAssignment(home, volunteerToAssign, [startDate, endDate])}
-								class="btn btn-success btn-xs btn-circle"><Plus /></button
-							>
+			{#if assignableHomes.length > 0}
+				<ul class="list mb-8">
+					<h2 class="subheading text-center mb-3">Assignable homes</h2>
+					{#each assignableHomes as home}
+						<li class="list-row">
+							{home.address1}
+							<div>
+								<div class="badge badge-xs">{home.distance_to_project} mi</div>
+								<button
+									onclick={() => createAssignment(home, volunteerToAssign, [startDate, endDate])}
+									class="btn btn-success btn-xs btn-circle"><Plus /></button
+								>
 
-							<details class="dropdown dropdown-end">
-								<summary class="btn btn-ghost btn-xs btn-circle m-1"><Dots /></summary>
-								<ul class="menu dropdown-content bg-base-300 rounded-box z-1 w-52 p-2 shadow-sm">
-									<li><a href="/homes/{home.id}">View Home</a></li>
-									<li>
-										<details>
-											<summary>Contact Host</summary>
-											<ul>
-												<li><a href="sms:{home.hosts.phone}">Text</a></li>
-												<li><a href="mailto:{home.hosts.email}">Email</a></li>
-												<li><a href="tel:{home.hosts.phone}">Call</a></li>
-											</ul>
-										</details>
-									</li>
-								</ul>
-							</details>
-						</div>
-					</li>
-				{/each}
-			</ul>
-		{/if}
+								<details class="dropdown dropdown-end">
+									<summary class="btn btn-ghost btn-xs btn-circle m-1"><Dots /></summary>
+									<ul class="menu dropdown-content bg-base-300 rounded-box z-1 w-52 p-2 shadow-sm">
+										<li><a href="/homes/{home.id}">View Home</a></li>
+										<li>
+											<details>
+												<summary>Contact Host</summary>
+												<ul>
+													<li><a href="sms:{home.hosts.phone}">Text</a></li>
+													<li><a href="mailto:{home.hosts.email}">Email</a></li>
+													<li><a href="tel:{home.hosts.phone}">Call</a></li>
+												</ul>
+											</details>
+										</li>
+									</ul>
+								</details>
+							</div>
+						</li>
+					{/each}
+				</ul>
+			{/if}
 
-		{#if unAssignableHomes.length > 0}
-			<ul class="list">
-				<p class="subheading">Unassignable homes</p>
+			{#if unAssignableHomes.length > 0}
+				<ul class="list">
+					<p class="subheading text-center mb-3">Unassignable homes</p>
 
-				{#each unAssignableHomes as badHome}
-					<li class="list-row">
-						{badHome.address1}
-						<div>
-							<div class="badge badge-xs">{badHome.distance_to_project} mi</div>
-							<details class="dropdown dropdown-end">
-								<summary class="btn btn-ghost btn-xs btn-circle m-1"><Dots /></summary>
-								<ul class="menu dropdown-content bg-base-300 rounded-box z-1 w-52 p-2 shadow-sm">
-									<li><a href="/homes/{badHome.id}">View Home</a></li>
-									<li>
-										<details>
-											<summary>Contact Host</summary>
-											<ul>
-												<li><a href="sms:{badHome.hosts.phone}">Text</a></li>
-												<li><a href="mailto:{badHome.hosts.email}">Email</a></li>
-												<li><a href="tel:{badHome.hosts.phone}">Call</a></li>
-											</ul>
-										</details>
-									</li>
-								</ul>
-							</details>
-						</div>
-					</li>
-				{/each}
-			</ul>
-		{/if}
+					{#each unAssignableHomes as badHome}
+						<li class="list-row">
+							{badHome.address1}
+							<div>
+								<div class="badge badge-xs">{badHome.distance_to_project} mi</div>
+								<details class="dropdown dropdown-end">
+									<summary class="btn btn-ghost btn-xs btn-circle m-1"><Dots /></summary>
+									<ul class="menu dropdown-content bg-base-300 rounded-box z-1 w-52 p-2 shadow-sm">
+										<li><a href="/homes/{badHome.id}">View Home</a></li>
+										<li>
+											<details>
+												<summary>Contact Host</summary>
+												<ul>
+													<li><a href="sms:{badHome.hosts.phone}">Text</a></li>
+													<li><a href="mailto:{badHome.hosts.email}">Email</a></li>
+													<li><a href="tel:{badHome.hosts.phone}">Call</a></li>
+												</ul>
+											</details>
+										</li>
+									</ul>
+								</details>
+							</div>
+						</li>
+					{/each}
+				</ul>
+			{/if}
 
-		<div class="modal-action">
-			<form method="dialog">
-				<button onclick={() => closeModal()} class="btn">Close</button>
-			</form>
-		</div>
+			<div class="modal-action">
+				<form method="dialog">
+					<button onclick={() => closeModal()} class="btn">Close</button>
+				</form>
+			</div>
+		{/await}
 	</div>
 </dialog>
 
