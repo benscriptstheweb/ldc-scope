ca0e6940af8dfff1e828c169df609ed043efb840
Robin Jalandoni <robinjalandoni@gmail.com>
2026-03-04 19:24:26 -0500

    style: increase spacing of homecards

--- src/components/HomeCard.svelte
+++ src/components/HomeCard.svelte
@@ -12,8 +12,8 @@
 </script>
 
 <div class="cards-container">
-	<button class="card cursor-pointer w-81 mt-1 text-left" onclick={visitHome}>
-		<div class="card-thing p-2 bg-base-200">
+	<button class="card cursor-pointer w-81 mt-3 text-left" onclick={visitHome}>
+		<div class="card-thing p-3 bg-base-200">
 			<div class="flex justify-between">
 				<div>
 					<div class="indicator card-title">
@@ -60,6 +60,6 @@
 		font-weight: 800;
 	}
 	.card-thing {
-		border-radius: 5px;
+		border-radius: 8px;
 	}
 </style>
