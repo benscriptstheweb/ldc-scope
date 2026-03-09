0ca78c2317c93563dd8a01335f6f7e1e3672239f
Robin Jalandoni <robinjalandoni@gmail.com>
2026-03-09 19:38:43 -0400

    feat: add reports page

---
+++ src/routes/reports/+page.server.ts
@@ -0,0 +1,25 @@
+import { supabase } from "$lib/supabase/supabaseClient";
+import { json } from "@sveltejs/kit";
+import type { PageServerLoad } from "../$types";
+
+export const load: PageServerLoad = async ({ fetch }) => {
+    const { data, error } = await supabase
+        .from('homes')
+        .select('city');
+
+    if (error) {
+        return json({ error: 'cannot get homes' }, { status: 500 })
+    }
+
+    const cityCounts = data.reduce((acc: any, home: any) => {
+        acc[home.city] = (acc[home.city] || 0) + 1;
+        return acc;
+    }, {});
+
+    return {
+        cities: Object.entries(cityCounts).map(([city, count]) => ({
+            city,
+            count
+        }))
+    }
+}
---
+++ src/routes/reports/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let { data } = $props();
+</script>
+
+<div class="mt-8 w-80 container flex flex-col">
+	<p class="ml-8 heading mb-8">Reports</p>
+
+	<div class="ml-5 collapse collapse-arrow bg-base-200 border border-base-300">
+		<input type="checkbox" name="my-accordion-2" />
+		<div class="collapse-title font-semibold">Number of homes by city</div>
+		<div class="collapse-content text-sm">
+			{#each data.cities as home}
+				<p>{home.city}: {home.count}</p>
+			{/each}
+		</div>
+	</div>
+</div>
