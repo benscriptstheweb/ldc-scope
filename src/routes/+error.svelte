d29a467f2ce65add56b99f7a2a5dadef2efce138
Robin Jalandoni <robinjalandoni@gmail.com>
2026-03-16 19:18:49 -0400

    style: prettier error pages

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import Tent from '../icons/Tent.svelte';
+</script>
+
+<div class="flex flex-col message text-center content-center mt-30">
+	<h1 class="mb-5">{page.status}</h1>
+	<Tent />
+	<p>({page.error?.message})</p>
+</div>
+
+<style>
+	h1 {
+		font-size: 3em;
+		font-weight: bold;
+	}
+</style>
