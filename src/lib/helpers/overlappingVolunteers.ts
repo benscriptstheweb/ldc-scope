1ceeba3b37d627e3476016e0475cb70cd25fcdc8
RJ <robinjalandoni@gmail.com>
2026-04-17 13:46:47 -0400

    Add console log for homeData in overlappingVolunteers

--- src/lib/helpers/overlappingVolunteers.ts
+++ src/lib/helpers/overlappingVolunteers.ts
@@ -23,10 +23,12 @@ export async function isOverlapping(home: any, dateRange: any) {
 		.eq('id', home.id)
 		.single();
 
+	console.log(homeData);
+
 	if (homeData.blackout_dates !== null) {
 		const hasBlackoutOnDateRange =
 			new Date(homeData.blackout_dates[0]).getTime() <= new Date(dateRange[1]).getTime() &&
-			new Date(homeData.blackout_dates[1]).getTime() >= new Date(dateRange[0]).getTime());
+			new Date(homeData.blackout_dates[1]).getTime() >= new Date(dateRange[0]).getTime();
 	
 	    if (hasBlackoutOnDateRange) {
 	        return true;
