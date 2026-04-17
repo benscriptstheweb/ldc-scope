b89c46f5599ea42b23ff8bcff6e6ae630bf8874c
RJ <robinjalandoni@gmail.com>
2026-04-17 12:39:45 -0400

    Refactor isOverlapping function to check assignments (#41)
    * Refactor isOverlapping function to check assignments
    
    remove overlaps due to inconsistency in data types
    
    * Comment out overlapping volunteers check
    
    Comment out the check for overlapping volunteers.
    
    * Fix date comparison logic in overlappingAssignments check
    
    * Fix overlapping assignments date comparison logic again
    
    * Log assignment data for overlapping checks
    
    Add logging for assignment data to debug overlapping assignments.
    
    * Fix date range check for overlapping assignments

--- src/lib/helpers/overlappingVolunteers.ts
+++ src/lib/helpers/overlappingVolunteers.ts
@@ -2,27 +2,31 @@ import { supabase } from '$lib/supabase/supabaseClient';
 import { type Assignment } from '$lib/supabase/types/assignment';
 
 export async function isOverlapping(home: any, dateRange: any) {
-	const { data } = await supabase
+	const { data: assignmentData } = await supabase
 		.from('assignments')
 		.select(`*`)
 		.eq('home_id', home.id)
-		.overlaps('date_range', dateRange)
 		.overrideTypes<Assignment[]>();
 
-	if (data && data.length > 0) {
-		return true;
-	}
+	// if (data && data.length > 0) {
+	// 	return true;
+	// }
 
 	// has blackout dates
 	const { data: homesData } = await supabase
 		.from('homes')
 		.select(`*`)
 		.eq('id', home.id)
-		.overlaps('blackout_dates', dateRange)
 
-	if (homesData && homesData.length > 0) {
-		return true;
-	}
+    const hasOverlappingAssignments = assignmentData?.some((assignment) => 
+		new Date(assignment.date_range[0]).getTime() <= new Date(dateRange[1]).getTime() &&
+		new Date(assignment.date_range[1]).getTime() >= new Date(dateRange[0]).getTime())
 
-	return false;
+	console.log(assignmentData);
+
+    if (hasOverlappingAssignments) {
+        return true;
+    }
+
+    return false;
 }
