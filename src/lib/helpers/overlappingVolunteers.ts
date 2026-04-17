ec1143ec143c175a79f56b8bd9b3fbff7d12c1f6
RJ <robinjalandoni@gmail.com>
2026-04-17 12:31:06 -0400

    Fix overlapping assignments date comparison logic again

--- src/lib/helpers/overlappingVolunteers.ts
+++ src/lib/helpers/overlappingVolunteers.ts
@@ -20,7 +20,7 @@ export async function isOverlapping(home: any, dateRange: any) {
 
     const hasOverlappingAssignments = assignmentData?.some((assignment) => 
 		new Date(assignment.date_start).getTime() <= new Date(dateRange[1]).getTime() &&
-		new Date(assignment.end_date).getTime() >= new Date(dateRange[0])).getTime()
+		new Date(assignment.end_date).getTime() >= new Date(dateRange[0]).getTime())
 
     if (hasOverlappingAssignments) {
         return true;
