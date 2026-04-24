cefecf9148c7e23b3650acc2b85428a7eafdefa1
RJ <robinjalandoni@gmail.com>
2026-04-24 10:29:40 -0400

    check assignments against length for 0

--- src/components/HomeAssignmentModal.svelte
+++ src/components/HomeAssignmentModal.svelte
@@ -44,7 +44,7 @@ console.log(dateRange);
 		
 		for (const home of homes) {	
 console.log(home);
-			const hasAssignmentOverlap = home.assignments && 
+			const hasAssignmentOverlap = home.assignments.length !== 0 && 
 				new Date(home.assignments.date_range[0]).getTime() <= new Date(dateRange[1]).getTime() &&
 				new Date(home.assignments.date_range[1]).getTime() >= new Date(dateRange[0]).getTime();
 
